230ba9580819f6afbe072c181190764b9b890fc4
Logan-Womack <loganswomack@gmail.com>
2025-07-05 14:55:09 -0400

    chore/feat adds tires alighments and defines frequency dictionary

---
+++ Main.py
@@ -0,0 +1,23 @@
+
+
+
+car_maintenance_tasks_frequency_dict = {
+    "Oil/Oil filter change": 5000,
+    "Tire Alignment": 12000,
+    "Tire rotation": 5000,
+    "Check windshield wiper condition": 5000,
+    "Top off fluids (washer, brake, coolant, etc.)": 5000,
+    "Check all exterior lights": 5000,
+    "Oil & oil filter change (synthetic oil)": 7500,
+    "Inspect air filter": 7500,
+    "Inspect brakes": 7500,
+    "Clean battery terminals": 7500,
+    "Replace cabin air filter": 15000,
+    "Replace engine air filter": 15000,
+    "Inspect belts and hoses": 15000,
+    "Replace transmission fluid (if required)": 30000,
+    "Inspect suspension and steering components": 30000,
+    "Replace spark plugs": 50000,
+    "Replace brake fluid": 50000,
+    "Replace timing belt (if applicable)": 75000,
+    "Replace coolant/antifreeze": 75000}
